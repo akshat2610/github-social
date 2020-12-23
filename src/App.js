3b257ebfb387967e6ac0bee6c517dfa28f7e9a20
Akshat Bansal <akshat2610@gmail.com>
2020-12-23 09:42:48 -0800

    Added graph for contributions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./containers/Main";
 import Form from "./components/form/Form";
 import Counter from "./components/counter/Counter";
 import Follower from "./components/follower/Follower";
+import Graph from "./components/graph/Graph";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,6 +18,7 @@ function App() {
         <Form setCount={setCount} setFollowers={setFollowers} setLoading={setLoading}/>
         <Counter count={count}/>
         <Follower followers={followers}/>
+        <Graph followers={followers} />
       </div>
     );
 }
