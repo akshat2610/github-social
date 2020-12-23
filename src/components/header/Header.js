08cca04518a4b3bfbc53d9fcdc25313d6c87e965
Akshat Bansal <akshat2610@gmail.com>
2020-12-23 12:35:34 -0800

    Added header and footer component

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+import "./Header.css";
+
+function Header() {
+  return(
+    <div id="header">
+      <h1> See how you rank among of those you follow in GitHub contributions! </h1>
+    </div>
+  );
+}
+export default Header;
